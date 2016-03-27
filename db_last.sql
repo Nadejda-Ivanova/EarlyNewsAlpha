68d9d0d466ecd9261039b640346b120b0e0d3451
boris999 <boris999@gmail.com>
2016-03-27 22:58:08 +0300

    Signed-off-by: boris999 <boris999@gmail.com>

--- db_last.sql
+++ db_last.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `news` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `news`;
 -- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
 -- Host: localhost    Database: news
@@ -100,4 +102,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-03-27 21:42:06
+-- Dump completed on 2016-03-27 22:55:18
